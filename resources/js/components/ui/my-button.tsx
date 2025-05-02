56f661f3d971c639d9c4f74472bf22c4ab756df3
Simon Vrachliotis <simon@simonswiss.com>
2025-05-02 12:56:14 +1000

    end of episode 06

---
+++ resources/js/components/ui/my-button.tsx
@@ -0,0 +1,44 @@
+import { cn } from '@/lib/utils';
+import { cva, VariantProps } from 'class-variance-authority';
+import { ComponentProps, ReactNode } from 'react';
+
+type MyButtonProps = {
+    children: ReactNode;
+    className?: string;
+} & VariantProps<typeof myButtonVariants> &
+    ComponentProps<'button'>;
+
+const myButtonVariants = cva('rounded-sm border border-transparent text-sm font-medium disabled:bg-slate-300 disabled:text-slate-500', {
+    variants: {
+        size: {
+            xl: 'px-7 py-4',
+            large: 'px-6 py-3',
+            medium: 'px-5 py-2',
+            small: 'px-4 py-1',
+        },
+        look: {
+            primary: 'bg-indigo-600 text-white hover:bg-indigo-500 active:bg-indigo-700',
+            secondary: 'bg-slate-200 text-slate-900 hover:bg-slate-300 active:bg-slate-400',
+            outline: 'border border-slate-300 bg-transparent text-slate-900 hover:bg-slate-100 active:bg-slate-200',
+        },
+    },
+    defaultVariants: {
+        size: 'medium',
+        look: 'primary',
+    },
+    compoundVariants: [
+        {
+            size: ['large', 'medium'],
+            look: ['outline', 'primary'],
+            className: 'uppercase',
+        },
+    ],
+});
+
+export function MyButton({ children, className = '', size, look, ...props }: MyButtonProps) {
+    return (
+        <button className={cn(myButtonVariants({ size, look, className }))} {...props}>
+            {children}
+        </button>
+    );
+}
--- resources/js/pages/welcome.tsx
+++ resources/js/pages/welcome.tsx
@@ -1,9 +1,33 @@
-import { Input } from '@/components/ui/input';
+import { Button, buttonVariants } from '@/components/ui/button';
+import { MyButton } from '@/components/ui/my-button';
 
 export default function Welcome() {
     return (
         <div className="min-h-screen p-4">
-            <Input placeholder="Start typing..." className="w-80" />
+            <div className="flex items-center gap-4">
+                <Button size="lg">Large</Button>
+                <Button>Button</Button>
+                <Button size="sm">Small</Button>
+                <Button variant="secondary">Secondary</Button>
+                <Button variant="outline">Outline</Button>
+                <Button variant="ghost">Ghost</Button>
+            </div>
+
+            {/* My Button */}
+            <div className="mt-4 flex flex-wrap items-center gap-4">
+                <MyButton size="xl">Extra Large</MyButton>
+                <MyButton size="large">Large</MyButton>
+                <MyButton>Medium</MyButton>
+                <MyButton size="small">Small</MyButton>
+                <MyButton look="secondary">Secondary</MyButton>
+                <MyButton look="outline">Outline</MyButton>
+                <MyButton size="large" look="outline">
+                    Outline
+                </MyButton>
+                <a className={buttonVariants({ variant: 'destructive' })} href="#">
+                    Button Link
+                </a>
+            </div>
         </div>
     );
 }
